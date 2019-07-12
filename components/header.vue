7bf2e535c246ad75daecdde48f8566db035db9d7
qiuqiu <306374476@qq.com>
2019-07-12 09:40:17 +0800

    first commit

---
+++ components/header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <el-row type="flex" class="header" justify="space-between">
+      <!-- logo  -->
+      <div class="logo">
+        <nuxt-link to="/">
+          <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+        </nuxt-link>
+      </div>
+      <!-- 头部内容部分 -->
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <!-- 登录 -->
+      <div>
+        <div v-if="false">
+          <nuxt-link to="/">登录/注册</nuxt-link>
+        </div>
+        <div>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <img src="http://157.122.54.189:9095/assets/images/avatar.jpg" alt />
+              首页
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+              <el-dropdown-item>退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.container {
+  height: 60px;
+  box-shadow: 0 3px 3px #ccc;
+  /* position: relative; */
+}
+.header {
+  width: 1000px;
+  margin: 0 auto;
+  line-height: 60px;
+}
+img {
+  width: 156px;
+  height: 42px;
+  margin-top: 8px;
+  margin-right: 12px;
+}
+.navs {
+  flex: 1;
+  a {
+    height: 60px;
+    padding: 0 20px;
+    &:hover {
+      color: rgb(64, 158, 255);
+      border-bottom: 5px solid rgb(64, 158, 255);
+    }
+  }
+  .nuxt-link-exact-active {
+    //高亮
+    background: rgb(64, 158, 255);
+    color: #fff;
+    &:hover {
+      color: #fff;
+    }
+  }
+}
+.el-dropdown-link {
+  img {
+    width: 36px;
+    height: 36px;
+    vertical-align: middle;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- 头部 -->
+    <Header/>
+    <nuxt />
+    <!-- 底部 -->
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+export default {
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*{
+  margin: 0;
+  padding:0;
+}
+ul,li,ol{
+  list-style: none;
+}
+em,i{
+  font-style: normal;
+}
+a{
+  color: inherit;
+  text-decoration:none;
+  box-sizing: border-box;
+}
+</style>
